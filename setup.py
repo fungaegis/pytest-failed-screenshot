af5fc42a0b7dfe7dcc4f3ce0b2b56e1825b3c972
petrisorionel <petrisor.ionel@gmail.com>
2021-04-21 22:00:21 +0300

    remove screenshot.py update setup.py
    remove screenshot.py update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 """
 author: fungaegis
 github: https://github.com/fungaegis/pytest-failed-screenshot
 """
+
 setup(
     name='pytest_failed_screenshot',
     url='https://github.com/fungaegis/pytest-failed-screenshot',
-    version='1.1.0',
+    version='1.0.2',
     author="fungaegis",
     author_email="fungaegis@gmail.com",
     description='Test case fails, take a screenshot, save it, attach it to the allure',
-    python_requires='>=3',
     classifiers=[
         'Framework :: Pytest',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Software Development :: Testing',
         'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Testing',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS :: MacOS X'
     ],
-    packages = find_packages(exclude=['tests', 'tests.*']),
-    keywords=['pytest', 'allure', 'screenshot', 'selenium', 'appium'],
+    license='MIT License',
+    py_modules=['pytest_failed_screenshot'],
+    keywords=[
+        'pytest', 'py.test', 'pytest_failed_screenshot', 'allure', 'screenshot', 'selenium', 'appium'
+    ],
+
     install_requires=[
         'pytest',
         'selenium',
@@ -32,8 +31,9 @@ setup(
         'allure-python-commons',
         'helium'
     ],
-    package_data={
-        'backend.screenshot': ['webdrivers/**/*']
-    },
-    test_suite='tests'
+    entry_points={
+        'pytest11': [
+            'failed_screenshot = pytest_failed_screenshot',
+        ]
+    }
 )
